8608cecb0f6b13689ad70b98e1cc5820ed28f58b
Adedayo Adegbola <71041211+Joesef127@users.noreply.github.com>
2023-05-23 02:50:32 +0100

    inspection commit

--- script.js
+++ script.js
@@ -1,34 +1,13 @@
-// document.onreadystatechange = function () {
-//   if (document.readyState === "complete") {
-//     hideLoadingScreen();
-//   }
-// };
-
-// function hideLoadingScreen() {
-//   const loadingScreen = document.getElementById("loading-screen");
-//   loadingScreen.style.opacity = "0";
-//   setTimeout(function() {
-//     loadingScreen.style.display = "none";
-//   }, 500);
-// }
-
-// window.addEventListener("load", function () {
-//   const progressBar = document.getElementById("progress-bar");
-//   let progress = 0;
-//   const interval = setInterval(function () {
-//     progress += 10;
-//     progressBar.style.width = progress + "%";
-//     if (progress >= 100) {
-//       clearInterval(interval);
-//     }
-//   }, 100);
-// });
 
 // embedded video
 function playVideo() {
   let video = document.getElementById('myVideo');
   if (video.paused) {
     video.play();
+    if(video.play) {
+      let playButton = document.getElementById('play-button')
+      playButton.style.display = "none"
+    }
   } else {
     video.pause();
   }
@@ -39,6 +18,6 @@ function playVideo() {
 const goTopBtn = document.querySelector("[dataScrollUp]");
 
 window.addEventListener("scroll", function () {
-  window.scrollY >= 1200 ? goTopBtn.classList.add("active")
+  window.scrollY >= 2000 ? goTopBtn.classList.add("active")
     : goTopBtn.classList.remove("active");
 });
